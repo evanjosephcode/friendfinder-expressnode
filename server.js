7c55b162d3c88c1b0672da4b322e5563b794a2a5
evanjosephcode <evanseaman89@gmail.com>
2018-04-23 23:05:50 -0500

    changed around filestructure

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, '/app/public')));
 
 // routing
 // require('./app/routing/apiRoutes.js')(app);
-require('./routing/htmlRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 
 // port listener 
