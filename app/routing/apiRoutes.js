8514189917aa2747d6184646611ba4e847f5001c
evanjosephcode <evanseaman89@gmail.com>
2018-04-25 18:09:21 -0500

    fixed apiRoutes to find match and for loop w/in for loop

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,30 +17,29 @@ module.exports = function apiRoutes(app) {
         let differenceArray = [];
         let newfriend = req.body;
         console.log(newfriend);
-        console.log("trying a new thing right now");
-        
+
         for (i = 0; i < friends.length; i++) {
             totalDifference = 0;
-            for (j = 0 ; j < newfriend.length; j++) {
+            for (j = 0; j < friends[i].scores.length; j++) {
                 totalDifference += Math.abs(friends[i].scores[j] - newfriend.scores[j]);
             }
+            console.log(totalDifference);
             differenceArray.push(totalDifference);
+
+
         }
+
+
 
         let match = differenceArray.indexOf(Math.min(...differenceArray));
         friends.push(newfriend);
+        console.log(match);
 
-        // fs.readFile(path.join(__dirname, '../data/friends.json'), 'utf8', (err, data) => {
-        //     if (err) throw err;
-        //     let json = JSON.parse(data);
-        //     json.push(newfriend);
-    
-        //     fs.writeFile(path.join(__dirname, '../data/friends.json'), JSON.stringify(json, null, 2), (err) => {
-        //       if (err) throw err;
-        //     });
-        // });
-    
-        // res.json(friends[match]);
+        res.json(friends[match]);
+
+
+
+
 
 
     });
