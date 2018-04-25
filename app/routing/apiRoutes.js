22086f7f7dd0d34c73dd1294c23a27cdfde0ab6f
evanjosephcode <evanseaman89@gmail.com>
2018-04-24 23:10:40 -0500

    added totalDifferences and newfriend to apiroutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,8 +16,31 @@ module.exports = function apiRoutes(app) {
         let totalDifference;
         let differenceArray = [];
         let newfriend = req.body;
-        // console.log(newfriend);
+        console.log(newfriend);
+        console.log("trying a new thing right now");
+        
+        for (i = 0; i < friends.length; i++) {
+            totalDifference = 0;
+            for (j = 0 ; j < newfriend.length; j++) {
+                totalDifference += Math.abs(friends[i].scores[j] - newfriend.scores[j]);
+            }
+            differenceArray.push(totalDifference);
+        }
+
+        let match = differenceArray.indexOf(Math.min(...differenceArray));
         friends.push(newfriend);
+
+        // fs.readFile(path.join(__dirname, '../data/friends.json'), 'utf8', (err, data) => {
+        //     if (err) throw err;
+        //     let json = JSON.parse(data);
+        //     json.push(newfriend);
+    
+        //     fs.writeFile(path.join(__dirname, '../data/friends.json'), JSON.stringify(json, null, 2), (err) => {
+        //       if (err) throw err;
+        //     });
+        // });
+    
+        // res.json(friends[match]);
 
 
     });
