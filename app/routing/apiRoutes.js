c6593c665d77f36ea8139986021bafba4f952c35
evanjosephcode <evanseaman89@gmail.com>
2018-04-30 23:24:59 -0500

    cleaned up code, removed console log and comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,26 +4,21 @@ module.exports = function apiRoutes(app) {
     const path = require("path");
     let friends = require("./../data/friends.js");
 
-    // console.log("stuff");
 
     app.get("/api/friends", (req, res) => {
         return res.json(friends);
-        // console.log("stuff");
-        // console.log(req.body);
     });
 
     app.post("/api/friends", (req, res) => {
         let totalDifference;
         let differenceArray = [];
         let newfriend = req.body;
-        console.log(newfriend);
 
         for (i = 0; i < friends.length; i++) {
             totalDifference = 0;
             for (j = 0; j < friends[i].scores.length; j++) {
                 totalDifference += Math.abs(friends[i].scores[j] - newfriend.scores[j]);
             }
-            console.log(totalDifference);
             differenceArray.push(totalDifference);
 
 
@@ -33,8 +28,6 @@ module.exports = function apiRoutes(app) {
 
         let match = differenceArray.indexOf(Math.min(...differenceArray));
         friends.push(newfriend);
-        console.log(match);
-
         res.json(friends[match]);
 
 
